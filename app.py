f2956a30d9dfb4cae95dd46c1bb0341631890e8a
zwd271828 <zachman321@gmail.com>
2021-03-08 22:00:59 +0000

    hi

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+import json
+import requests
+
 app = Flask(__name__)
+url = "https://ws.api.video/auth/api-key"
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+d = {
+    "apiKey": 'ZdSBGXrlXi5FrJYoYlg0uSAe7q4L8LpQC3mSRrwDtBF'
+}
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+   r = requests.post(url, data=json.dumps(d), headers=headers)
+   return json.loads(r.text)['access_token']
+
