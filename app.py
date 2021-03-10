7d29c37ebe4f31d3030143db9a7acc8347eea43f
zwd271828 <zachman321@gmail.com>
2021-03-10 22:16:02 +0000

    lel

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 import json
 import mux_python
 
+
 app = Flask(__name__)
 
 MUX_TOKEN_ID='bbcb8911-562b-4b0a-bf4a-64de6bad9ffc'
@@ -13,14 +14,16 @@ configuration.password = MUX_TOKEN_SECRET
 
 uploads_api = mux_python.DirectUploadsApi(mux_python.ApiClient(configuration))
 
-create_asset_request = mux_python.CreateAssetRequest(playback_policy=[mux_python.PlaybackPolicy.PUBLIC])
-create_upload_request = mux_python.CreateUploadRequest(timeout=3600, new_asset_settings=create_asset_request, cors_origin="*")
-create_upload_response = uploads_api.create_direct_upload(create_upload_request)  
 
-@app.route('/url')
+@app.route('/')
 def hello_world():
-   return create_upload_response.data.url
-
-@app.route('/id')
-def id_world():
-   return create_upload_response.data.id
+   create_asset_request = mux_python.CreateAssetRequest(playback_policy=[mux_python.PlaybackPolicy.PUBLIC])
+   create_upload_request = mux_python.CreateUploadRequest(timeout=3600, new_asset_settings=create_asset_request, cors_origin="*")
+   create_upload_response = uploads_api.create_direct_upload(create_upload_request)  
+   print(create_upload_response)
+   d = {
+    "url": create_upload_response.data.url,
+    "id": create_upload_response.data.id
+   }
+   return json.dumps(d)
+   
